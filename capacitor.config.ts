2c151280b4545d364e24084f0988e485498e3a84
Rahoolsharma8 <132122761+Rahoolsharma8@users.noreply.github.com>
2026-02-22 12:10:30 +0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.umpk.attendance',
+  appName: 'attendx-app',
+  webDir: 'build'
+};
+
+export default config;
